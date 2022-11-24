9bc6a477f3bf329197e28685bdc6f0a789895af2
verhovensky <105872686+verhovenskyivan@users.noreply.github.com>
2022-11-24 22:02:41 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Kata
+{
+    public static int[] CountPositivesSumNegatives(int[] input)
+    {
+        if (input == null || !input.Any())
+        {
+            return new int[] { };
+        }
+
+        int countPositives = input.Count(n => n > 0);
+        int sumNegatives = input.Where(n => n < 0).Sum();
+
+        return new int[] { countPositives, sumNegatives };
+    }
+}
